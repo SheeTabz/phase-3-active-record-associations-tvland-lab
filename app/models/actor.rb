73af9fdac1ea98fbaa9dfff02e0c3df578a952b2
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-02 15:06:15 +0300

    Completed assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    actr1 = self.first_name + ' ' + self.last_name
+    actr1
+  end
+
+  def list_roles
+    self.shows.map do |show|
+        self.characters.map do |character|
+            res ="#{character.name} - #{show.name}"
+            
+        end.join("\n")
+    end
   
+  end
 end
